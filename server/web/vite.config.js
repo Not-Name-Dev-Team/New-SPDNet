50f10897d8736d3fe64dbbc250bbe879b3d1586e
catand <3047354896@qq.com>
2026-03-03 02:11:00 +0800

    修改端口到80

--- server/web/vite.config.js
+++ server/web/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:15447',
+        target: 'http://localhost:80',
         changeOrigin: true
       }
     }
